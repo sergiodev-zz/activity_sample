3a80061518e1ca3042345ebb6a8c1259ed896ffd
sergiodev <ing.florio@gmail.com>
2019-06-26 20:59:51 +0200

    s

--- activity.go
+++ activity.go
@@ -4,6 +4,7 @@ import (
 	"github.com/d2r2/go-dht"
 	"github.com/project-flogo/core/activity"
 	"github.com/project-flogo/core/data/metadata"
+	"github.com/stianeikeland/go-rpio"
 )
 
 func init() {
@@ -48,13 +49,13 @@ func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
 
 	ctx.Logger().Debugf("Input: %s", input.AnInput)
 
-	// error := rpio.Open()
-	// if error != nil {
-	// 	return true, err
-	// }
-	// pin := rpio.Pin(17)
-	// pin.Input() // Input mode
-	pin := 11
+	error := rpio.Open()
+	if error != nil {
+		return true, err
+	}
+	pin := rpio.Pin(17)
+	pin.Input() // Input mode
+	// pin := 11
 	sensorType := dht.DHT11
 	temperature, humidity, retried, err := dht.ReadDHTxxWithRetry(sensorType, pin, false, 10)
 	if err != nil {
