d878487ab5c384319b6ba06f7af91f3314715ac1
sergiodev <ing.florio@gmail.com>
2019-06-26 21:47:49 +0200

    a

--- activity.go
+++ activity.go
@@ -1,8 +1,6 @@
 package sample
 
 import (
-	"strconv"
-
 	"github.com/project-flogo/core/activity"
 	"github.com/project-flogo/core/data/metadata"
 	"github.com/stianeikeland/go-rpio"
@@ -78,9 +76,9 @@ func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
 
 	// temp := strconv.FormatFloat(temperature, 'f', 6, 64)
 	// humy := strconv.FormatFloat(humidity, 'f', 6, 64)
-	rsString := strconv.FormatFloat(res, 'f', 6, 64)
+	// rsString := strconv.FormatFloat(res, 'f', 6, 64)
 
-	output := &Output{AnOutput: res}
+	output := &Output{AnOutput: "res"}
 	err = ctx.SetOutputObject(output)
 	if err != nil {
 		return true, err
