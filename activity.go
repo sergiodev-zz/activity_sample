acfecceeef81a2eb2616f907a7102fa2b8173e6f
sergiodev <ing.florio@gmail.com>
2019-06-27 00:03:38 +0200

    funzionante

--- activity.go
+++ activity.go
@@ -66,7 +66,7 @@ func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
 		return true, err
 	}
 
-	dht, err := dht.NewDHT("GPIO19", dht.Fahrenheit, "")
+	dht, err := dht.NewDHT("GPIO19", dht.Celsius, "")
 	if err != nil {
 		return true, err
 	}
